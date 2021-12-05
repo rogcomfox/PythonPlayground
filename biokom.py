94ec73bec9fcfe0983d4c9edff5ec6d35af192d7
rogcomfox <joeg.11021996@gmail.com>
2021-12-05 20:23:16 +0700

    add question and change log message

--- biokom.py
+++ biokom.py
@@ -15,7 +15,7 @@ def checkCondition():
     
     # check second condition
     while True:
-        secCon = str(input("Apakah Anda Mengalami Batuk? [ya/tidak]")).capitalize()
+        secCon = str(input("Apakah Anda Mengalami Sesak Napas? [ya/tidak]")).capitalize()
         if secCon == yes.capitalize() or secCon == no.capitalize():
             break
         else:
@@ -23,23 +23,23 @@ def checkCondition():
     
     # check third condition
     while True:
-        thirdCon = str(input("Apakah Anda Mengalami Nyeri Dada? [ya/tidak]")).capitalize()
+        thirdCon = str(input("Apakah Anda Mengalami Batuk? [ya/tidak]")).capitalize()
         if thirdCon == yes.capitalize() or thirdCon == no.capitalize():
             break
         else:
             print("Input salah, silahkan masukkan ya atau tidak")
-
+    
     # check fourth condition
     while True:
-        fourthCon = str(input("Apakah Anda Mengalami Gejala Mual Muntah? [ya/tidak]")).capitalize()
+        fourthCon = str(input("Apakah Anda Mengalami Nyeri Dada? [ya/tidak]")).capitalize()
         if fourthCon == yes.capitalize() or fourthCon == no.capitalize():
             break
         else:
             print("Input salah, silahkan masukkan ya atau tidak")
-    
+
     # check fifth condition
     while True:
-        fifthCon = str(input("Apakah Anda Mengalami Kelelahan? [ya/tidak]")).capitalize()
+        fifthCon = str(input("Apakah Anda Mengalami Gejala Mual Muntah? [ya/tidak]")).capitalize()
         if fifthCon == yes.capitalize() or fifthCon == no.capitalize():
             break
         else:
@@ -47,20 +47,28 @@ def checkCondition():
     
     # check sixth condition
     while True:
-        sixthCon = str(input("Apakah Anda Mengalami KGB Membengkak? [ya/tidak]")).capitalize()
+        sixthCon = str(input("Apakah Anda Mengalami Kelelahan? [ya/tidak]")).capitalize()
         if sixthCon == yes.capitalize() or sixthCon == no.capitalize():
             break
         else:
             print("Input salah, silahkan masukkan ya atau tidak")
     
-    if firstCon == yes.capitalize() and secCon == yes.capitalize() and thirdCon == yes.capitalize()  and fourthCon == yes.capitalize()  and fifthCon == yes.capitalize()  and sixthCon == yes.capitalize() :
-        # check seventh condition
+    # check seventh condition
+    while True:
+        sevCon = str(input("Apakah Anda Mengalami Kelenjar Getah Bening Membengkak? [ya/tidak]")).capitalize()
+        if sevCon == yes.capitalize() or sevCon == no.capitalize():
+            break
+        else:
+            print("Input salah, silahkan masukkan ya atau tidak")
+    
+    if firstCon == yes.capitalize() and secCon == yes.capitalize() and thirdCon == yes.capitalize() and fourthCon == yes.capitalize() and fifthCon == yes.capitalize() and sixthCon == yes.capitalize() and sevCon == yes.capitalize() :
+        # check eight condition
         while True:
-            sevCon = str(input("Apakah Anda Pernah Kontak Dengan Pasien Covid? [ya/tidak]")).capitalize()
-            if sevCon == yes.capitalize():
+            eiCon = str(input("Apakah Anda Pernah Kontak Dengan Pasien Covid? [ya/tidak]")).capitalize()
+            if eiCon == yes.capitalize():
                 covid("iya")
                 break
-            elif sevCon == no.capitalize():
+            elif eiCon == no.capitalize():
                 covid("tidak")
                 break
             else:
@@ -71,7 +79,7 @@ def checkCondition():
 def bronkitis():
     print("Nama Pasien: ", patientInfo[0])
     print("\nJenis Kelamin: ", patientInfo[1])
-    print("\nAnda Diwajibkan Melakukan Langkah Pemeriksaan Bronkitis")
+    print("\nPenyakit pernapasan disebabkan oleh banyak kemungkinan. Apabila Anda mengalami salah satu gejala tersebut, segera lakukan pemeriksaan ke dokter untuk pemeriksaan lebih lanjut. Apabila tidak mengalami gejala apapun, Anda sehat.")
 
 def covid(condition):
     print("Nama Pasien: ", patientInfo[0])
@@ -79,7 +87,7 @@ def covid(condition):
     if condition == "iya".casefold():
         print("\nAnda Diwajibkan Melakukan Swab")
     elif condition == "tidak".casefold():
-        print("\nAnda Diwajibkan Melakukan Langkah Pemeriksaan Pneumonia")
+        print("\nSilakan lakukan pemeriksaan ke dokter dengan melalukukan foto toraks atau dada")
 
 if __name__ == "__main__":
     print("Selamat Datang!")
